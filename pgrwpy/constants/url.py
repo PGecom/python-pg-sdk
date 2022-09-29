e623d5fb4b41699985a4151d8f4387264e07d2c9
Esterling Accime <accimeesterlin@yahoo.com>
2022-09-29 09:43:19 -0700

    update production urlc

--- pgrwpy/constants/url.py
+++ pgrwpy/constants/url.py
@@ -1,6 +1,6 @@
 class URL(object):
     SANDBOX_BASE_URL = 'https://devtopup.pgecom.com/api/'
-    PRODUCTION_BASE_URL = 'https://pgecom.com/api/'
+    PRODUCTION_BASE_URL = 'https://topup.pgecom.com/api/'
     RESOLVE = 'https://help.pgecom.com/en/'
     MONCASH = "moncash/token"
     CARD = "card/"
